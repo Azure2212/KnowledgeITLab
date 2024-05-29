46bc960cb945b6a81bb6a4863c4b67d1161e8186
Azure2212 <dasanbob22122002@gmail.com>
2024-05-29 22:51:46 +0700

    initial

---
+++ src/main/java/com/example/springboot_github_mvc/SpringbootGithubMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_github_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_github_mvc/SpringbootGithubMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_github_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
